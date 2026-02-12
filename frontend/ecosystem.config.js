a5c49d2497cd884c6ce2bf79a056111afeec07ba
Aleksandr Cherenev <amcherenev@cloud.ru>
2026-02-12 10:46:49 +0300

    feat: deploy

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -6,9 +6,7 @@ dotenv.config({
   path: path.resolve(__dirname, '.env.deploy'),
 });
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REPOSITORY, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
-} =
-  process.env;
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REP, DEPLOY_PATH, DEPLOY_REF } = process.env;
 
 module.exports = {
   deploy: {
@@ -16,7 +14,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: DEPLOY_REPOSITORY,
+      repo: DEPLOY_REP,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp -Cr .env.production ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/shared`,
       'pre-deploy': `cp ${DEPLOY_PATH}/shared/.env.production ${DEPLOY_PATH}/current/frontend/.env.production`,
